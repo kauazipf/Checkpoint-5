36cb303231f4f08c65709e68ae7731c275199675
kauazipf <rm558957@fiap.com.br>
2024-10-04 10:52:52 -0300

    Ajustes na estilização das paginas.
    Co-authored-by: Victor Egidio Lira <Vitin46@users.noreply.github.com>
    Co-authored-by: marcelooou <marcelooou@users.noreply.github.com>

--- cp5/src/app/calculo/page.tsx
+++ cp5/src/app/calculo/page.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import Image from "next/image"
 
 export default function Calculo() {
     return (
-        <div className="container mx-auto p-8 m-40">
-            <h1 className="text-3xl font-bold mb-4">Cálculo</h1>
-            <p className="text-lg">
-                Fórmula: <span className="font-mono">F = G * (m₁ * m₂) / r²</span>
-            </p>
-            <p className="mt-4 text-lg">
-                Onde:
-                <ul className="list-disc list-inside mt-2">
-                    <li><strong>F</strong> é a força gravitacional entre dois corpos</li>
-                    <li><strong>G</strong> é a constante gravitacional</li>
-                    <li><strong>m₁</strong> e <strong>m₂</strong> são as massas dos corpos</li>
-                    <li><strong>r</strong> é a distância entre os centros dos dois corpos</li>
-                </ul>
-            </p>
+        <div className="container mx-auto p-8 flex flex-col items-center justify-center max-h-screen gap-16 m-16">
+            <div>
+                <h1 className="text-3xl font-bold mb-4">Cálculo</h1>
+                <p className="text-lg">
+                    Fórmula: <span className="font-mono">F = G * (m₁ * m₂) / r²</span>
+                </p>
+                <p className="mt-4 text-lg">
+                    Onde:
+                    <ul className="list-disc list-inside mt-2 leading-relaxed ">
+                        <li><strong>F</strong> é a força gravitacional entre dois corpos</li>
+                        <li><strong>G</strong> é a constante gravitacional</li>
+                        <li><strong>m₁</strong> e <strong>m₂</strong> são as massas dos corpos</li>
+                        <li><strong>r</strong> é a distância entre os centros dos dois corpos</li>
+                    </ul>
+                </p>
+            </div>
+            <Image src={"/images/calculo.png"} alt='Formula da gravidade' width={300} height={300}></Image>
         </div>
     );
 }
--- cp5/src/app/livro/page.tsx
+++ cp5/src/app/livro/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Livro() {
   return (
-    <div className="container mx-auto p-8 flex flex-col items-center justify-center min-h-screen gap-10">
+    <div className="container mx-auto p-8 flex flex-col items-center justify-center max-h-screen gap-10">
       <h1 className="text-4xl font-bold mb-4 text-center">Worlds in Collision</h1>
       
       <Image src={"/images/livro.png"} alt='Immanuel Velikovsky' width={300} height={300}></Image>
