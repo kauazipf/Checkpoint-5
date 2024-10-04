19dbea06206cd9960c4390d1d9eab7281b949772
kauazipf <rm558957@fiap.com.br>
2024-10-04 10:30:40 -0300

    Realização da pagina do planeta venus.
    Co-authored-by: Victor Egidio Lira <Vitin46@users.noreply.github.com>
    Co-authored-by: marcelooou <marcelooou@users.noreply.github.com>

--- cp5/src/app/not-found.tsx
+++ cp5/src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function NotFound() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
-        <Image src={"/img/error.png"} alt='Erro 404' width={1000} height={1000}></Image>
+        <Image src={"/images/error.png"} alt='Erro 404' width={1000} height={1000}></Image>
     </div>
   )
 }
---
+++ cp5/src/app/planeta/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from "next/image"
+
+export default function Venus() {
+    return (
+        <div className="container mx-auto p-8 flex flex-col items-center justify-center max-h-screen gap-24">
+            <h1 className="text-4xl font-bold mb-4 text-center">Teoria do Nascimento de Venus</h1>
+
+            <Image src={"/images/venus.png"} alt='Venus' width={330} height={330}></Image>
+            <p className="text-lg leading-relaxed text-center">
+                Velikovsky acreditava que Vênus era um cometa que se separou de Júpiter e passou perto de outros objetos no Sistema Solar. Ele sugeriu que os gases quentes do cometa causaram as 10 pragas do Egito Antigo.
+
+            </p>
+
+        </div>
+    );
+}
